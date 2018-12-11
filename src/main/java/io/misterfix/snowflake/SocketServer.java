962f150f44977593be997f02e62b940b79e38070
Mister Fix <me@mc-heads.net>
2018-12-12 00:37:59 +0200

    Removed memory limit to eliminate bottlenecks, made the thing serve snowflakes, and beginning of stats collection.

--- src/main/java/io/misterfix/snowflake/SocketServer.java
+++ src/main/java/io/misterfix/snowflake/SocketServer.java
@@ -14,11 +14,8 @@ import joptsimple.OptionSpec;
 import java.net.InetSocketAddress;
 
 public class SocketServer extends ChannelInboundHandlerAdapter {
-    private final Snowflake snowflake;
-
-    private SocketServer(Snowflake snowflake) {
-        this.snowflake = snowflake;
-    }
+    private static Snowflake snowflake;
+    private static long ids_served; //for future use, do not remove
 
     public static void main(String[] args) throws InterruptedException {
         OptionParser parser = new OptionParser();
@@ -27,6 +24,9 @@ public class SocketServer extends ChannelInboundHandlerAdapter {
         OptionSpec<Integer> datacenterId = parser.accepts("datacenter-id").withRequiredArg().ofType(Integer.class).defaultsTo(1);
         OptionSpec<Integer> instanceId = parser.accepts("instance-id").withRequiredArg().ofType(Integer.class).defaultsTo(1);
         OptionSet set = parser.parse(args);
+        //The snowflake object only needs to be created once, otherwise duplicate ID's are served
+        snowflake = new Snowflake(set.valueOf(datacenterId), set.valueOf(instanceId));
+
 
         EventLoopGroup group = new NioEventLoopGroup();
         try {
@@ -34,9 +34,11 @@ public class SocketServer extends ChannelInboundHandlerAdapter {
             serverBootstrap.group(group);
             serverBootstrap.channel(NioServerSocketChannel.class);
             serverBootstrap.localAddress(new InetSocketAddress("localhost", set.valueOf(port)));
+            System.out.println("Snowflake server started on port "+set.valueOf(port)+".");
             serverBootstrap.childHandler(new ChannelInitializer<SocketChannel>() {
                 protected void initChannel(SocketChannel socketChannel) {
-                    socketChannel.pipeline().addLast(new SocketServer(new Snowflake(set.valueOf(datacenterId), set.valueOf(instanceId))));
+                    socketChannel.writeAndFlush(Unpooled.copiedBuffer(String.valueOf(snowflake.nextId()), CharsetUtil.UTF_8)).addListener(ChannelFutureListener.CLOSE);
+                    ids_served++;
                 }
             });
             ChannelFuture channelFuture = serverBootstrap.bind().sync();
@@ -46,16 +48,6 @@ public class SocketServer extends ChannelInboundHandlerAdapter {
         } finally {
             group.shutdownGracefully().sync();
         }
-    }
-
-    @Override
-    public void channelRead(ChannelHandlerContext ctx, Object msg) {
-        ctx.write(Unpooled.copiedBuffer(String.valueOf(snowflake.nextId()), CharsetUtil.UTF_8));
-    }
-
-    @Override
-    public void channelReadComplete(ChannelHandlerContext ctx) {
-        ctx.writeAndFlush(Unpooled.EMPTY_BUFFER).addListener(ChannelFutureListener.CLOSE);
     }
 
     @Override
