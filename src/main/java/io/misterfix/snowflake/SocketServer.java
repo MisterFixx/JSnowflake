f5606d1dd9b52ed119d491417f6192f8bcf601c2
Mister Fix <me@mc-heads.net>
2018-12-26 21:35:27 +0200

    Started switching from Netty to SimpleNet

--- src/main/java/io/misterfix/snowflake/SocketServer.java
+++ src/main/java/io/misterfix/snowflake/SocketServer.java
@@ -1,23 +1,17 @@
 package io.misterfix.snowflake;
 
-import io.netty.bootstrap.ServerBootstrap;
-import io.netty.buffer.Unpooled;
-import io.netty.channel.*;
-import io.netty.channel.nio.NioEventLoopGroup;
-import io.netty.channel.socket.SocketChannel;
-import io.netty.channel.socket.nio.NioServerSocketChannel;
-import io.netty.util.CharsetUtil;
+
 import joptsimple.OptionParser;
 import joptsimple.OptionSet;
 import joptsimple.OptionSpec;
+import simplenet.Server;
+import simplenet.packet.Packet;
 
-import java.net.InetSocketAddress;
-
-public class SocketServer extends ChannelInboundHandlerAdapter {
+public class SocketServer{
     private static Snowflake snowflake;
     private static long ids_served;
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         OptionParser parser = new OptionParser();
         parser.allowsUnrecognizedOptions();
         OptionSpec<Integer> port = parser.accepts("port").withRequiredArg().ofType(Integer.class).defaultsTo(9098);
@@ -30,37 +24,19 @@ public class SocketServer extends ChannelInboundHandlerAdapter {
         OptionSet set = parser.parse(args);
 
         snowflake = new Snowflake(set.valueOf(datacenterId), set.valueOf(instanceId), set.valueOf(epoch));
-        EventLoopGroup group = new NioEventLoopGroup(80);
         new AdminService(set.valueOf(adminServicePort), set.valueOf(instanceId), set.valueOf(datacenterId), set.valueOf(adminServiceUser), set.valueOf(adminServicePass), set.valueOf(epoch));
 
-        try {
-            ServerBootstrap serverBootstrap = new ServerBootstrap();
-            serverBootstrap.group(group);
-            serverBootstrap.channel(NioServerSocketChannel.class);
-            serverBootstrap.localAddress(new InetSocketAddress("localhost", set.valueOf(port)));
-            System.out.println("Snowflake service started on port "+set.valueOf(port)+".");
-            System.out.println("Snowflake Admin service started on port "+set.valueOf(adminServicePort)+".");
-            serverBootstrap.childHandler(new ChannelInitializer<SocketChannel>() {
-                public void initChannel(SocketChannel socketChannel) {
-                    socketChannel.writeAndFlush(Unpooled.copiedBuffer(String.valueOf(snowflake.nextId()), CharsetUtil.UTF_8)).addListener(ChannelFutureListener.CLOSE);
-                    ids_served++;
-                }
-            });
-            ChannelFuture channelFuture = serverBootstrap.bind().sync();
-            channelFuture.channel().closeFuture().sync();
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-        }
-        finally {
-            group.shutdownGracefully().sync();
-        }
-    }
+        Server server = new Server();
+        server.onConnect(client -> client.readByteAlways(value -> {
+            System.out.println(value);
+            Packet.builder().putLong(snowflake.nextId()).writeAndFlush(client);
+            ids_served++;
+        }));
+        server.bind("localhost", set.valueOf(port));
 
-    @Override
-    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
-        cause.printStackTrace();
-        ctx.close();
+        System.out.println("Snowflake service started on port "+set.valueOf(port)+".");
+        System.out.println("Snowflake Admin service started on port "+set.valueOf(adminServicePort)+".");
+
     }
 
     static long getIdsServed(){
